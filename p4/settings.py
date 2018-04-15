f853fa293d8d71254d12ebc195738a79c1618b3e
vvshabrat <vvshabrat@gmail.com>
2018-04-15 12:37:41 +0300

    !

--- p4/settings.py
+++ p4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'djcq3%$l!v1#w%cdr&c@5-4b3d!lw#^)!ohyo*@**9@dkvjx%f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'vvsha.pythonanywhere.com']
 
 
 # Application definition
