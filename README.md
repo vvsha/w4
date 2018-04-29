f9bb258eafe7a65b1bbe8d1e270ec486d3a7cd1f
vvshabrat <vvshabrat@gmail.com>
2018-04-29 19:30:28 +0300

    !

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# w4
+(djangogirls-admin)
+
+http://127.0.0.1:8000/
+Hello, Viva!
+
+http://127.0.0.1:8000/admin
+
+see png-files
--- a4/views.py
+++ a4/views.py
@@ -4,4 +4,3 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'a4/post_list.html', {})
-	
